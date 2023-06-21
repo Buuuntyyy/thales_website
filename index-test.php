d04f49444a3023592a0161ac37288f4c6b6a087e
Buuuntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2023-06-22 01:24:33 +0200

    totally works, don't touch

--- index-test.php
+++ index-test.php
@@ -1,7 +1,26 @@
 <?php
+    $lignes_aff = 100;
+    if(isset($_POST['nb_lignes'])){
+        $lignes_aff = $_POST['nb_lignes'];
+        setcookie("lignes_aff", $_POST['nb_lignes'], (time()+365*24*3600)*10);
+    }
+    elseif (isset($_POST['nb_lignes_h'])) {
+        $lignes_aff_h = $_POST['nb_lignes_h'];
+        setcookie("lignes_aff_h", $lignes_aff_h, (time()+365*24*3600)*10);
+
+    }
+    else{
+        if(isset($_COOKIE['lignes_aff_h'])){
+            $lignes_aff_h = $_COOKIE['lignes_aff_h'];
+        }
+        else{
+            setcookie("lignes_aff_h", 10, (time()+365*24*3600)*10);
+            $lignes_aff_h = 10;
+        }
+    }
     @$keywords=$_GET["keywords"];
     if (empty($keywords)){
-        $keywords = "test";
+        $keywords = "V";
     }
     @$valider=$_GET["valider"];
     if (isset($valider) && !empty(trim($keywords))){
@@ -32,47 +51,50 @@
 <body>
     <header>
         <h1>Sniffer-Ethernet</h1>
+        <?php print_r($_COOKIE); ?>
         <img class="logo-groupe" src="logo-groupe.jpeg" alt="Logo groupe">
     </header>
     <div class="container">
 
         <div class="box1">
-            <h3>Historique des 10 derniers Tests</h3> <!--afficher ici les 10 derniers test ainsi qu'une 
+
+            <?php echo "<h3>Historique des {$_COOKIE['lignes_aff_h']} dernieres exécutions</h3>" ?><!--afficher ici les 10 derniers test ainsi qu'une 
                                 barre de recherche pour aller chercher un test spécifique dans la BDD-->
+            
             <?php
-            try {
-                $bd = new PDO ( "mysql:host=localhost;dbname=thales",
-                "root", "" );
-                $bd->exec('SET NAMES utf8');
-            }
-            catch (Exception $e) {
-                die ("Erreur: Connexion à la base impossible");
-            }
-
-            $sql = "SELECT nom_test, date FROM test"; // Stocke le code SQL de la requête
-            $req = $bd->prepare ($sql); // Requête préparée
-            $req->execute (); // Requête exécutée
-            $lesEnreg = $req->fetchall (); // Requête exécutée
-            $req->closeCursor (); // Requête détruite
-
-            ?>
-            <br>
-            <br>
-            <?php for($i=0;$i<sizeof($lesEnreg);$i++){ ?>
-                <li><?php echo "<a href=resultats.php/>{$lesEnreg[$i]['nom_test']} {$lesEnreg[$i]['date']}</a>"?> </li>
-            <?php } ?>
+                try {
+                    $bd = new PDO ( "mysql:host=localhost;dbname=thales",
+                    "root", "" );
+                    $bd->exec('SET NAMES utf8');
+                }
+                catch (Exception $e) {
+                    die ("Erreur: Connexion à la base impossible");
+                }
+        
+                $sql = "SELECT id_test, date_exec, exec_id from execution
+                ORDER BY exec_id DESC
+                LIMIT 10; "; // Stocke le code SQL de la requête
+                $req = $bd->prepare ($sql); // Requête préparée
+                $req->execute (); // Requête exécutée
+                $LesExecs = $req->fetchall (); // Requête exécutée
+                $req->closeCursor (); // Requête détruite
+                for ($w=0;$w<sizeof($LesExecs);$w++){
+                    echo "<a href='resultats.php?exec_id={$LesExecs[$w]['exec_id']}&pageid=0&test_page={$_COOKIE['lignes_aff']}'>{$LesExecs[$w]['exec_id']} {$LesExecs[$w]['date_exec']}</a>";
+                    ?> <br> <?php
+                }
+                ?><br>
         </div>
         <div class="box2">
             <h3>détails et infos</h3>
-            <form action="index.html" method="POST" id="onglet-parametres">
+            <form action="#" method="POST" id="onglet-parametres">
                 <label for="nb_lignes">Nombre d'affichage de lignes par page:</label>
                 <input type="number" id="nb_lignes" name="nb_lignes" min="1" max="1000" value="100">
                 <input type="submit" value="Enregistrer">
             </form>
                 <br>
-            <form action="index.html" method="POST" id="onglet-parametres">
-                <label for="nb_lignes">Nombre d'affichage historique</label>
-                <input type="number" id="nb_lignes" name="nb_lignes" min="1" max="1000" value="100">
+            <form action="#" method="POST" id="onglet-parametres">
+                <label for="nb_lignes_h">Nombre d'affichage historique</label>
+                <input type="number" id="nb_lignes_h" name="nb_lignes_h" min="1" max="1000" value="10">
                 <input type="submit" value="Enregistrer">
             </form>
             
@@ -107,8 +129,7 @@
                     for ($w=0;$w<sizeof($LesExecs);$w++){
 
                         if ($LesExecs[$w]['id_test'] == $tab_recherche[$i]['test_id']){
-
-                            echo "<a href='resultats.php?exec_id={$LesExecs[$w]['exec_id']}&pageid=0'>{$LesExecs[$w]['exec_id']} {$LesExecs[$w]['date_exec']}</a>";
+                            echo "<a href='resultats.php?exec_id={$LesExecs[$w]['exec_id']}&pageid=0&test_page={$_COOKIE['lignes_aff']}'>{$LesExecs[$w]['exec_id']} {$LesExecs[$w]['date_exec']}</a>";
                             ?> <br> <?php
                         }
                     }
--- resultats.php
+++ resultats.php
@@ -1,16 +1,13 @@
-<?php
+<?php    
     if (!isset($_COOKIE['nom_col']))
     {
-        #echo"putain";
         $name_col = array("field1", "field2", "field3", "field4", "field5", "field6", "field7", "field8", "field9", "field10", "field11", "field12", "field13", "field14", "field15", "field16", "field17", "field18", "field19", "field20", "field21", "field22", "field23", "field24", "field25", "field26", "field27", "field28", "field29", "field30", "field31", "field32", "field33", "field34");
         setcookie("nom_col", serialize($name_col), (time()+365*24*3600)*10);
-        #echo "oh noooooooooooooooo";
         $init=0;
     }
     else{
         $test = unserialize($_COOKIE['nom_col']);
         $init = 1;
-        #print_r($_COOKIE);
         print_r($test);
         if (!empty($_POST)){
             $modif = $_POST;
@@ -32,7 +29,7 @@
     }
     try {
         $bd = new PDO ( "mysql:host=localhost;dbname=thales",
-        "root", "" );
+        "root", "");
         $bd->exec('SET NAMES utf8');
         echo "bdddddddddddddddddddddddd";
     }
@@ -74,14 +71,17 @@
                 <td><input type="submit" value="enregistrer" /></td>
                 </tr>
             </form>
-            <?php 
+            <?php
+                $test = $_GET['test_page'];
                 if (!isset($_GET['max'])){
-                    $max_val = 100;
+                    $max_val = $test;
+                    $max_val = $_GET['test_page'];
+                    $seuil = 0;
                 }
                 else{
                     $max_val = $_GET['max'];
+                    $seuil = $max_val - $test;
                 }
-                $seuil = $max_val - 100;
                 for($i=$seuil;$i<=$max_val;$i++){
                     echo "<tr>";
                     echo "<td> {$i} </td>";
@@ -100,29 +100,38 @@
                 $urlb = $url . strval($before);
             }
             else if (!isset($_GET['max'])){
-                $max = 100;
+                echo "okkkkkkkkkk";
+                $max = 2 * $test;
                 $next = $_GET['pageid'] + 1;
-                $url = substr($_SERVER['REQUEST_URI'], 0, - strlen(strval($_GET['pageid'])));
-                $urln = $url . strval($next) . "&max={$max}";
+                $t_param =  strlen(strval($_GET['pageid'])) + 11 + strlen(strval($test));
+                $url = substr($_SERVER['REQUEST_URI'], 0, - $t_param);
+                $urln = $url . strval($next) . "&test_page={$test}" . "&max={$max}";
             }
             else{
+                echo "test";
+                echo $test;
+                echo "============";
+                $max = $test + $_GET['max'];
+                echo $max;
                 $before = $_GET['pageid'] - 1;
                 if ($before <= 0){
                     $before = 0;
                 }
-                $taille_param =  strlen(strval($_GET['pageid'])) + 5 + strlen(strval($_GET['max']));
+                $taille_param =  strlen(strval($_GET['pageid'])) + 11 + strlen(strval($test)) + 5 + strlen(strval($_GET['max']));
                 $url = substr($_SERVER['REQUEST_URI'], 0, - $taille_param);
                 ?><br><?php
-                $max_avant = $i - 100;
+                $max_avant = $max - 2 * $test;
                 if ($max_avant < 100){
-                    $max_avant = 100;
+                    $max_avant = $test;
                 }
-                $urlb = $url . strval($before) . "&max={$max_avant}";
-                $max = $i + 100;
+                $urlb = $url . strval($before) . "&test_page={$test}" . "&max={$max_avant}";
                 $next = $_GET['pageid'] + 1;
-                $taille_param =  strlen(strval($_GET['pageid'])) + 5 + strlen(strval($_GET['max']));
-                $url = substr($_SERVER['REQUEST_URI'], 0, - $taille_param);
-                $urln = $url . strval($next) . "&max={$max}";
+                #$taille_param =  strlen(strval($_GET['pageid'])) + 5 + strlen(strval($_GET['max']));
+                #$url = substr($_SERVER['REQUEST_URI'], 0, - $taille_param);
+                echo "url";
+                echo $url;
+                $urln = $url . strval($next) . "&test_page={$test}" . "&max={$max}";
+                echo $urln;
             }
         ?>
         <?php echo "<p> <a href='{$urlb}'>&lt;</a> Test bas de page <a href='{$urln}'>&gt;</a> </p>"; ?>
